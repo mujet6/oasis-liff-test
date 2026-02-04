13389f27057c38a33ee27a65363bd9f87c3460ae
mujet6 <mugu.jet@gmail.com>
2026-02-04 11:09:28 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,29 @@
+const LIFF_ID = "2009043945-xeksuOQk";  // ←あなたのIDに置き換え済
+
+async function main() {
+  await liff.init({ liffId: LIFF_ID });
+
+  if (!liff.isLoggedIn()) {
+    liff.login();
+    return;
+  }
+
+  document.getElementById("send").addEventListener("click", async () => {
+
+    if (!liff.isInClient()) {
+      alert("LINE内で開いてください");
+      return;
+    }
+
+    const msg = document.getElementById("msg").value;
+
+    await liff.sendMessages([
+      { type: "text", text: "テスト送信：" + msg }
+    ]);
+
+    alert("送信完了");
+    liff.closeWindow();
+  });
+}
+
+main();
